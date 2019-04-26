973b6b49b491a54e8ed35abe71b7547287f0f44b
shanluoMu <49744759+shanluoMu@users.noreply.github.com>
2019-04-26 14:55:47 +0800

    Update tests.ts

--- tests.ts
+++ tests.ts
@@ -1,11 +1,1 @@
-namespace DFRobot_PN532{
-    /*是否检测到卡片*/
-    //% weight = 60
-    //% blockId=isDetection block="检测到卡片？"  
-    export function IsDetection(): boolean {
-        if (1)
-            return true
-        else
-            return false
-    }
-}
+
